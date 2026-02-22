c9c5bfd54acd1910c49ba293468f15bf6ef62584
Lucio Cabrera <luciocabrera1978@gmail.com>
2026-02-22 11:37:04 +0100

    feat: enhance column sorting logic in useSetColumnSorting hook; improve normalization and state management

--- src/components/Table/contexts/TableConfig/columns/actions/useSetColumnSorting.hook.ts
+++ src/components/Table/contexts/TableConfig/columns/actions/useSetColumnSorting.hook.ts
@@ -4,6 +4,7 @@ import { useSearchParams } from 'react-router';
 import type { Sorting } from '@/types/ui.types';
 
 import { useTableConfigContextValue } from '@/components/Table/contexts/TableConfig/useTableConfigContextValue.hook';
+import { getNormalizedColummns } from '@/components/Table/utils';
 import { writeStateSlice } from '@/components/Table/utils/writeStateSlice.util';
 
 export const useSetColumnSorting = <TData>() => {
@@ -17,19 +18,34 @@ export const useSetColumnSorting = <TData>() => {
       const columnsState = columnsStore.get();
       const sorting = columnsState?.sorting ?? [];
       const currentSort = sorting.find((s) => s.columnKey === columnKey);
+      const hasCurrentSort = currentSort !== undefined;
 
       if (currentSort?.direction === direction) {
         // No change in sort
         return;
       }
-      const newSorting = sorting
-        .map((s) => {
-          if (s.columnKey === columnKey) {
-            return { columnKey, direction };
-          }
-          return s;
-        })
-        .filter((s) => s.direction); // Remove any with undefined direction
+
+      let newSorting = [...sorting];
+
+      if (hasCurrentSort) {
+        newSorting =
+          direction === undefined
+            ? sorting.filter((s) => s.columnKey !== columnKey)
+            : sorting.map((s) => {
+                if (s.columnKey === columnKey) {
+                  return { columnKey, direction };
+                }
+
+                return s;
+              });
+      } else if (direction !== undefined) {
+        newSorting = [...sorting, { columnKey, direction }];
+      }
+
+      const normalizedColumns = getNormalizedColummns({
+        columns: columnsState?.columns ?? [],
+        sorting: newSorting,
+      });
 
       writeStateSlice({
         persistenceKey,
@@ -45,7 +61,7 @@ export const useSetColumnSorting = <TData>() => {
         }
         return params;
       });
-      columnsStore.set({ sorting: newSorting });
+      columnsStore.set({ normalizedColumns, sorting: newSorting });
     },
     [columnsStore, persistenceKey, setSearchParams],
   );
