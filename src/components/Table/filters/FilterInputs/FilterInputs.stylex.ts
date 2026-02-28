3c9b9b9e709bc9e474e272b7aa613eafb69fcb1c
Lucio Cabrera <luciocabrera1978@gmail.com>
2026-02-28 17:37:56 +0100

    feat: add operatorOverride style to FilterInputs for improved dropdown appearance

--- src/components/Table/filters/FilterInputs/FilterInputs.stylex.ts
+++ src/components/Table/filters/FilterInputs/FilterInputs.stylex.ts
@@ -1,6 +1,22 @@
+import * as stylex from '@stylexjs/stylex';
+
 import { filterBaseStyles } from '@/design-system/tokens/filters.stylex';
+
+const localStyles = stylex.create({
+  operatorOverride: {
+    borderRadius: 0,
+    overflow: 'visible',
+    backgroundColor: 'transparent',
+    boxShadow: 'none',
+    position: 'relative',
+    left: 'auto',
+    right: 'auto',
+    top: 'auto',
+  },
+});
 
 export const styles = {
   container: filterBaseStyles.container,
+  operatorOverride: localStyles.operatorOverride,
   select: filterBaseStyles.select,
 };
