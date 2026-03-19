34bda8da14c3db1c0ecec13c1e4bac2417a15d86
Lucio Cabrera <luciocabrera1978@gmail.com>
2026-03-19 18:53:07 +0100

    refactor: remove unused HandleToggleVisibilityArgs type from ColumnOrderSection.types.ts

--- src/components/Table/TableSettingsDrawer/ColumnOrderSection/ColumnOrderSection.types.ts
+++ src/components/Table/TableSettingsDrawer/ColumnOrderSection/ColumnOrderSection.types.ts
@@ -2,11 +2,6 @@ import type { ComponentPropsWithoutRef } from 'react';
 
 export type ColumnOrderSectionProps = ComponentPropsWithoutRef<'div'>;
 
-export type HandleToggleVisibilityArgs = {
-  columnKey: string;
-  isVisible: boolean;
-};
-
 export type OrderConflictResolution =
   | 'pin-to-match-order'
   | 'remove-conflicting-pins'
