cbc733ff0d62ec097807cf692139d5091ec18a33
Lucio Cabrera <luciocabrera1978@gmail.com>
2026-03-20 11:07:11 +0100

    feat: Add type exports for Modal, NavLink, and Toolbar components

--- src/components/Modal/index.ts
+++ src/components/Modal/index.ts
@@ -1,1 +1,2 @@
 export { Modal } from './Modal.component';
+export type { ModalProps } from './Modal.types';
--- src/components/Toolbar/index.ts
+++ src/components/Toolbar/index.ts
@@ -1,1 +1,7 @@
 export { Toolbar } from './Toolbar.component';
+export type {
+  ToolbarButtonConfig,
+  ToolbarItemConfig,
+  ToolbarLinkConfig,
+  ToolbarProps,
+} from './Toolbar.types';
