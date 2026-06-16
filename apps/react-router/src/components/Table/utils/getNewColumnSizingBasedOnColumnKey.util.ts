0d36ea8336ff6bcb7bfa525d417c27e3e37fe50e
Lucio Cabrera <luciocabrera1978@gmail.com>
2026-06-16 20:03:26 +0200

    feat: refactor getNewColumnSizingBasedOnColumnKey utility to improve clarity and maintainability

--- apps/react-router/src/components/Table/utils/getNewColumnSizingBasedOnColumnKey.util.ts
+++ apps/react-router/src/components/Table/utils/getNewColumnSizingBasedOnColumnKey.util.ts
@@ -12,13 +12,28 @@ export const getNewColumnSizingBasedOnColumnKey = <TData>({
   columnSizesState = {} as ColumnSizingState<TData>,
 }: GetNewColumnSizingBasedOnColumnKeyArgs<TData>): ColumnSizingState<TData> => {
   // Sizing: remove this column entry, then re-add if size exists
-  const baseSizing = Object.fromEntries(
-    Object.entries(columnSizesState).filter(([key]) => key !== columnKey),
-  );
-  const newColumnSizing =
-    columnSizing === undefined
-      ? baseSizing
-      : { ...baseSizing, [columnKey]: columnSizing };
+  // const baseSizing = Object.fromEntries(
+  //   Object.entries(columnSizesState).filter(([key]) => key !== columnKey),
+  // );
+  // const newColumnSizing =
+  //   columnSizing === undefined
+  //     ? baseSizing
+  //     : { ...baseSizing, [columnKey]: columnSizing };
 
-  return newColumnSizing as ColumnSizingState<TData>;
+  // return newColumnSizing as ColumnSizingState<TData>;
+
+  const keys = Object.keys(
+    columnSizesState,
+  ) as (keyof ColumnSizingState<TData>)[];
+
+  const initial = (
+    columnSizing === undefined ? {} : { [columnKey]: columnSizing }
+  ) as ColumnSizingState<TData>;
+
+  return keys.reduce<ColumnSizingState<TData>>((acc, k) => {
+    if (k !== (columnKey as keyof ColumnSizingState<TData>)) {
+      acc[k] = columnSizesState[k];
+    }
+    return acc;
+  }, initial);
 };
