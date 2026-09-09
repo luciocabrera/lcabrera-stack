f7b64885d2acec762d8c95d8137a58d12924a8e7
Lucio Cabrera <luciocabrera1978@gmail.com>
2026-09-09 22:21:50 +0200

    fix(ui): stop re-exporting store hydration helpers
    Import ProvideStoreContext and syncStoreFromProps from their modules.
    The hooks/utils barrel had no consumers for those re-exports.

--- packages/ui/src/hooks/utils/index.ts
+++ packages/ui/src/hooks/utils/index.ts
@@ -1,4 +1,2 @@
 export { getVerticalVirtualizationWindow } from './getVerticalVirtualizationWindow.util';
-export { ProvideStoreContext } from './provideStoreContext.util';
 export { setupObservedContainer } from './setupObservedContainer.util';
-export { syncStoreFromProps } from './syncStoreFromProps.util';
